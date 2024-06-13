ea10b9284b101ac33c7afa7e146a324b38792a45
Kevin Gugelmann <kevin.gugelmann@outlook.com>
2024-06-13 10:34:50 +0100

    Chip animations: entrance, shake

--- src/components/react/chip.tsx
+++ src/components/react/chip.tsx
@@ -2,15 +2,21 @@ interface ChipProps {
 	text: string;
 	isSelected: boolean;
 	onClick: () => void | undefined;
+	isShaking?: boolean;
+	disabled?: boolean;
 }
 
 export default function Chip(props: ChipProps) {
 	return (
 		<button
-			className='chip'
+			className={`${props.isSelected ? 'bg-yellow-700' : 'bg-slate-700'} 
+			${props.isShaking ? 'shake' : ''}
+			mr-2 mb-1.5 px-2 py-0.5
+			transition duration-300 ease-in-out transform hover:scale-105`}
 			onClick={props.onClick}
+			disabled={props.disabled}
 		>
-			{props.isSelected ? '●' : ''} {props.text} {' '}
+			{props.text}
 		</button>
 	)
 }
--- src/components/react/infinite-craft.tsx
+++ src/components/react/infinite-craft.tsx
@@ -3,6 +3,7 @@ import { EmojiWord } from "../../cloudstate/emoji-word";
 import type { InfiniteCraftState } from "../../cloudstate/infinite-craft";
 import Chip from "./chip";
 import { useState } from "react";
+import { CSSTransition, TransitionGroup } from "react-transition-group";
 import { QueryClient, QueryClientProvider, useMutation, useQuery } from "@tanstack/react-query";
 
 interface InfiniteCraftAppProps {
@@ -14,6 +15,23 @@ export default function InfiniteCraftApp(props: InfiniteCraftAppProps) {
 	return (
 		<QueryClientProvider client={queryClient}>
 			<ChipList {...props} />
+			<style>{`
+				  .chip-container-enter {
+					transform: scale(0.8);
+					opacity: 0;
+				  }
+				  .shake {
+					animation: shake 0.5s;
+				  }
+				  @keyframes shake {
+					0% { transform: translate(1px, 1px) rotate(0deg); }
+					20% { transform: translate(-1px, -2px) rotate(-1deg); }
+					40% { transform: translate(-3px, 0px) rotate(1deg); }
+					60% { transform: translate(3px, 2px) rotate(0deg); }
+					80% { transform: translate(1px, -1px) rotate(1deg); }
+					100% { transform: translate(1px, 1px) rotate(0deg); }
+				  }
+            `}</style>
 		</QueryClientProvider>
 	)
 }
@@ -28,13 +46,28 @@ function ChipList(props: InfiniteCraftAppProps) {
 	});
 	const { isPending: isCrafting, mutate: craftWord } = useMutation({
 		mutationFn: ({ a, b }: { a: EmojiWord, b: EmojiWord }) => icState.craftWord(a, b),
-		// onError: (error) => console.error('Error crafting word:', error),
-		onSuccess: () => refetch(),
+		onSuccess: (emojiWord) => {
+			// Reset selected chips
+			setSelectedIdxs([]);
+
+			// Shake crafted word
+			const idx = words.findIndex(word => word.text === emojiWord.text);
+			setShakingIdx(idx);
+			setTimeout(() => setShakingIdx(null), 500);
+
+			// Refetch words
+			refetch();
+		},
+		onError: (error) => {
+			console.error('Error crafting word:', error);
+			setSelectedIdxs([]);
+		}
+		,
 	});
 
 	const [selectedIdxs, setSelectedIdxs] = useState<number[]>([]);
+	const [shakingIdx, setShakingIdx] = useState<number | null>(null);
 	const selectChip = (idx: number) => {
-		console.log('selectChip', idx);
 		let newSelectedIdxs: number[];
 
 		if (selectedIdxs.includes(idx)) {
@@ -45,30 +78,33 @@ function ChipList(props: InfiniteCraftAppProps) {
 			newSelectedIdxs = [...selectedIdxs, idx];
 		}
 
-		console.log('newSelectedIdxs', newSelectedIdxs)
-
 		if (newSelectedIdxs.length === 2) {
 			// Two chips selected: craft word
 			const [a, b] = newSelectedIdxs.map(selectedIdx => words[selectedIdx]);
 			craftWord({ a, b });
-
-			// Reset selected chips
-			newSelectedIdxs = [];
 		}
-		console.log('newSelectedIdxs', newSelectedIdxs)
+
 		setSelectedIdxs(newSelectedIdxs);
 	}
 
 	return (
-		<div className="chip-list">
-			{words.map((word, idx) =>
-				<Chip
-					key={idx}
-					text={`${word.emoji} ${word.text}`}
-					isSelected={selectedIdxs.includes(idx)}
-					onClick={() => { selectChip(idx) }}
-				/>
-			)}
+		<div>
+			<div className="chip-list mx-6 my-4">
+				<TransitionGroup>
+					{words.map((word, idx) =>
+						<CSSTransition key={idx} timeout={300} classNames="chip-container">
+							<Chip
+								text={`${word.emoji} ${word.text}`}
+								isSelected={selectedIdxs.includes(idx)}
+								onClick={() => { selectChip(idx) }}
+								isShaking={shakingIdx === idx}
+								disabled={isCrafting}
+							/>
+						</CSSTransition>
+					)}
+				</TransitionGroup>
+			</div>
+			{isCrafting && <div className="text-center text-xl mt-8">Crafting...</div>}
 		</div>
 	);
 }
