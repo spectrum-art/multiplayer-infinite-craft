d072a409d78b202df7d7e54afe9863fc7c0dd45f
Benjamin Swerdlow <Swerdlowbenjamin@gmail.com>
2024-06-25 12:16:04 -0700

    fix

--- src/components/game.tsx
+++ src/components/game.tsx
@@ -4,7 +4,7 @@ import "../styles/app.css";
 
 import { EmojiNoun, EmojiNounRes } from "../cloudstate/noun";
 import toast, { Toaster } from "react-hot-toast";
-import Chip from "./Chip";
+import Chip from "./chip";
 import { useCloud } from "freestyle-sh";
 import { RoomCS } from "../cloudstate/room";
 import { QueryClient, QueryClientProvider, useQuery, useMutation } from "@tanstack/react-query";
